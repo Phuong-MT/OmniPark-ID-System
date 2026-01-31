e219d917f54abe3b8b04851e44cdd75be22f3342
minh phuong <minhphuongyenky@gmail.com>
2026-02-01 00:53:56 +0700

    chore: structure esp-gate

---
+++ devices/esp32-gate/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("ESP32 Gate Controller Booting...");
+}
+
+void loop() {
+  Serial.println("System Running...");
+  delay(2000);
+}
