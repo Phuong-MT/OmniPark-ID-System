0003a471af6033c956db1153476d86d56efe887a
minh phuong <minhphuongyenky@gmail.com>
2026-02-04 23:51:23 +0700

    chore: swagger-api-docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Omnipark ID System')
+    .setDescription('The Omnipark ID System API description')
+    .setVersion('0.1')
+    .addCookieAuth('authCookie')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, { ignoreGlobalPrefix: true });
+  SwaggerModule.setup('api-docs', app, documentFactory, {
+    swaggerOptions: {
+      requestInterceptor: (req) => {
+        req.credentials = 'include';
+        return req;
+      },
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
